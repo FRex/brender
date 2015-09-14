2816ff7eb1c192658761df764e6204fdbd3da188
frex <rex2k8@gmail.com>
2015-09-15 00:19:01 +0200

    Made each vertex transform once only.

--- src/renderMesh.cpp
+++ src/renderMesh.cpp
@@ -10,12 +10,15 @@ const float halfscreenheight = 480.f / 2.f;
 void draw(Rasterizer& raster, const Mesh& mesh, const arma::mat44& mat, std::vector<BufferedVertice>& buff)
 {
     const unsigned * idxs = mesh.getIndices();
-    unsigned idxc = mesh.getIndexCount();
+    const unsigned idxc = mesh.getIndexCount();
+    const unsigned vc = mesh.getVertexCount();
     arma::vec4 av;
-
-    for(int i = 0; i < idxc; ++i)
+    
+    buff.clear();
+    
+    for(int i = 0; i < vc; ++i)
     {
-        const Vertex& a = mesh.getVertex(idxs[i]);
+        const Vertex& a = mesh.getVertex(i);
 
         av(0) = a.position.x;
         av(1) = a.position.y;
@@ -26,7 +29,13 @@ void draw(Rasterizer& raster, const Mesh& mesh, const arma::mat44& mat, std::vec
         av(0) = av(0) * (kProjPlaneDist / std::fabs(av(2))) + halfscreenwidth;
         av(1) = av(1) * (kProjPlaneDist / std::fabs(av(2))) + halfscreenheight;
 
-        raster.addVertex(av(0), av(1), a.color, av(2), a.u, a.v);
+        buff.push_back(BufferedVertice(av(0), av(1), a.color, av(2), a.u, a.v));
+
     }//for
 
+    for(int i = 0; i < idxc; ++i)
+    {
+        const BufferedVertice&v = buff[idxs[i]];
+        raster.addVertex(v.x, v.y, v.c, v.d, v.u, v.v);
+    }
 }
